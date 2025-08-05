868d77b275fc93b8e4bc9d137328218c8a7ca46d
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-08-05 19:58:00 +0530

    Commit

---
+++ server/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.use((req, res, next) => {
+    console.log('Routes: Api')
+});
+
+router.get('') 
+
+const apiRouter = router
+module.exports = apiRouter;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
+const apiRouter = require('./routes');
 const port = 3000;
+
+dotenv.config();
+
+app.use(cors({
+    origin: process.env.CLIENT_DOMAIN,  // Frontend URL
+    credentials: true // Allow cookies and authorization headers
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    console.log('req.method :>> ', req.method);
+    console.log('req.url :>> ', req.url);
+})
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello World'})
+})
+
+app.use('api', apiRouter);
 
 app.listen(port, (err) => {
     if (!err) {
